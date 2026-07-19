4cf93b481dac8527712267fbc0c0c06de93fa9cf
Bidhan Dhakal <bidhan206482@gmail.com>
2026-07-19 10:37:18 +0545

    Initial portfolio setup

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen items-center justify-center bg-black text-white">
+      <h1 className="text-5xl font-bold">
+        Hello, I'm Bidhan 👋
+      </h1>
+    </main>
+  );
+}
